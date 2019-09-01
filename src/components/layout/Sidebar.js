9b0f6387edcc46b3d4372299843941c4fe99052c
daviwesley <davi.wesley@icloud.com>
2019-09-01 17:41:26 -0300

    add provider

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,41 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaChevronDown,
   FaInbox,
-  FaRegCalendar,
-  FaRegCalendarAlt
+  FaRegCalendarAlt,
+  FaRegCalendar
 } from "react-icons/fa";
+import { useSelectedProjectValue } from "../../context";
+
 export const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue();
+  const [active, setActive] = useState("inbox");
+  const [showProjects, setShowProjects] = useState(true);
+
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
-        <li>
-          <span>
-            <FaInbox />
-          </span>
-          <span>Inbox</span>
+        <li data-testid="inbox" className={"inbox"}>
+          <div data-testid="inbox-action">
+            <span>
+              <FaInbox />
+            </span>
+            <span>Inbox</span>
+          </div>
         </li>
-        <li>
-          <span>
-            <FaRegCalendar />
-          </span>
-          <span>Today</span>
+        <li data-testid="today" className={"today"}>
+          <div data-testid="today-action">
+            <span>
+              <FaRegCalendar />
+            </span>
+            <span>Today</span>
+          </div>
         </li>
-        <li>
-          <span>
-            <FaRegCalendarAlt />
-          </span>
-          <span>Next 7 days</span>
+        <li data-testid="next_7" className={"next_7"}>
+          <div
+            data-testid="next_7-action"
+            aria-label="Show tasks for the next 7 days"
+          >
+            <span>
+              <FaRegCalendarAlt />
+            </span>
+            <span>Next 7 days</span>
+          </div>
         </li>
       </ul>
       <div className="sidebar__middle">
         <span>
-          <FaChevronDown />
+          <FaChevronDown
+            className={!showProjects ? "hidden-projects" : undefined}
+          />
         </span>
         <h2>Projects</h2>
       </div>
-      <ul className="sidebar__projects">Projects will be here</ul>
-      Add project component here!!
+
+      <ul className="sidebar__projects"></ul>
     </div>
   );
 };
